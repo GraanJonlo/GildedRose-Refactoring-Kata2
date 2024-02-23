b0fb3c7c4e36fb76ab460dedeedc59da85127e85
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 13:52:32 +0000

    refactor: Lift conditionbal to extract aged brie specific logic

--- GildedRose/WrappedItem.cs
+++ GildedRose/WrappedItem.cs
@@ -4,55 +4,65 @@ public class WrappedItem(Item item)
 {
     public void UpdateItem()
     {
-        if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert")
+        if (item.Name == "Aged Brie")
         {
             if (item.Quality < 50)
             {
                 item.Quality++;
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.SellIn < 11)
-                    {
-                        IncreaseQuality();
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        IncreaseQuality();
-                    }
-                }
             }
-        }
-        else
-        {
-            if (item.Quality > 0)
-            {
-                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                {
-                }
-                else
-                {
-                    item.Quality--;
-                }
-            }
-        }
 
-        if (item.Name == "Sulfuras, Hand of Ragnaros")
-        {
-        }
-        else
-        {
             item.SellIn--;
-        }
 
-        if (item.SellIn < 0)
-        {
-            if (item.Name == "Aged Brie")
+            if (item.SellIn < 0)
             {
                 IncreaseQuality();
             }
+        }
+        else
+        {
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            IncreaseQuality();
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            IncreaseQuality();
+                        }
+                    }
+                }
+            }
             else
+            {
+                if (item.Quality > 0)
+                {
+                    if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    {
+                    }
+                    else
+                    {
+                        item.Quality--;
+                    }
+                }
+            }
+
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+            }
+            else
+            {
+                item.SellIn--;
+            }
+
+            if (item.SellIn < 0)
             {
                 if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {

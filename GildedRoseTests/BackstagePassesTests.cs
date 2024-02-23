935d2f655f8bcef806b5676d477afe45a6c37e7d
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 15:54:48 +0000

    refactor: Move backstage passes tests to own file

---
+++ GildedRoseTests/BackstagePassesTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class BackstagePassesTests
+{
+    [TestCase(2, 1)]
+    [TestCase(1, 0)]
+    [TestCase(0, -1)]
+    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
+    {
+        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
+    }
+
+    [TestCase(48, 49)]
+    [TestCase(49, 50)]
+    public void ItShouldIncreaseQuality(int initialQuality, int expectedQuality)
+    {
+        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = initialQuality };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [TestCase(10)]
+    [TestCase(6)]
+    public void ItShouldIncreaseQualityByTwoWhenSellInIsTenOrLess(int initialSellIn)
+    {
+        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(2));
+    }
+
+    [TestCase(5)]
+    [TestCase(1)]
+    public void ItShouldIncreaseQualityByThreeWhenSellInIsFiveOrLess(int initialSellIn)
+    {
+        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void ItShouldDropQualityToZeroAfterTheConcert()
+    {
+        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 50 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(0));
+    }
+}
--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -42,69 +42,6 @@ Just for clarification, an item can never have its Quality increase above 50, ho
 a legendary item and as such its Quality is 80, and it never alters.
  */
 
-public class BackstagePassesTests
-{
-    [TestCase(2, 1)]
-    [TestCase(1, 0)]
-    [TestCase(0, -1)]
-    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
-    {
-        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = initialSellIn, Quality = 0 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
-    }
-
-    [TestCase(48, 49)]
-    [TestCase(49, 50)]
-    public void ItShouldIncreaseQuality(int initialQuality, int expectedQuality)
-    {
-        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = initialQuality };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
-    }
-
-    [TestCase(10)]
-    [TestCase(6)]
-    public void ItShouldIncreaseQualityByTwoWhenSellInIsTenOrLess(int initialSellIn)
-    {
-        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = initialSellIn, Quality = 0 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(2));
-    }
-
-    [TestCase(5)]
-    [TestCase(1)]
-    public void ItShouldIncreaseQualityByThreeWhenSellInIsFiveOrLess(int initialSellIn)
-    {
-        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = initialSellIn, Quality = 0 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(3));
-    }
-
-    [Test]
-    public void ItShouldDropQualityToZeroAfterTheConcert()
-    {
-        Item item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 50 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(0));
-    }
-}
-
 public class GildedRoseTest
 {
     [Test]

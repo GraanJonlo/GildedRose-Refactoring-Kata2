df85a89bdbb92fe804561e9f307e8c9774420b35
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 13:13:23 +0000

    test: Add tests for how aged brie quality changes

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -56,6 +56,40 @@ public class AgedBrieTests
 
         Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
     }
+
+    [TestCase(48, 49)]
+    [TestCase(49, 50)]
+    public void ItShouldIncreaseQuality(int initialQuality, int expectedQuality)
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = 1, Quality = initialQuality };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [Test]
+    public void ItShouldIncreaseInQualityTwiceAsFastWhenPastSellIn()
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void ItShouldNotIncreaseQualityAbove50()
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(50));
+    }
 }
 
 public class GildedRoseTest

04fc3536023c0232c255c31ca0dd93ded37e6fb5
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 16:35:44 +0000

    refactor: Move conjured item tests to own file

---
+++ GildedRoseTests/ConjuredItemTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class ConjuredItemTests
+{
+    [TestCase(2, 1)]
+    [TestCase(1, 0)]
+    [TestCase(0, -1)]
+    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
+    {
+        Item item = new Item { Name = "Conjured", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
+    }
+
+    [TestCase(4, 2)]
+    [TestCase(2, 0)]
+    public void ItShouldDecreaseQualityTwiceAsFast(int initialQuality, int expectedQuality)
+    {
+        Item item = new Item { Name = "Conjured", SellIn = 1, Quality = initialQuality };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [Test]
+    public void ItShouldNotDecreaseQualityBelowZero()
+    {
+        Item item = new Item { Name = "Conjured", SellIn = 1, Quality = 1 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(0));
+    }
+
+    [TestCase(8, 4)]
+    [TestCase(4, 0)]
+    public void ItShouldDecreaseQualityTwiceAsFastAfterSellIn(int initialQuality, int expectedQuality)
+    {
+        Item item = new Item { Name = "Conjured", SellIn = 0, Quality = initialQuality };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+}
--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -42,57 +42,6 @@ Just for clarification, an item can never have its Quality increase above 50, ho
 a legendary item and as such its Quality is 80, and it never alters.
  */
 
-public class ConjuredItemTests
-{
-    [TestCase(2, 1)]
-    [TestCase(1, 0)]
-    [TestCase(0, -1)]
-    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
-    {
-        Item item = new Item { Name = "Conjured", SellIn = initialSellIn, Quality = 0 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
-    }
-
-    [TestCase(4, 2)]
-    [TestCase(2, 0)]
-    public void ItShouldDecreaseQualityTwiceAsFast(int initialQuality, int expectedQuality)
-    {
-        Item item = new Item { Name = "Conjured", SellIn = 1, Quality = initialQuality };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
-    }
-
-    [Test]
-    public void ItShouldNotDecreaseQualityBelowZero()
-    {
-        Item item = new Item { Name = "Conjured", SellIn = 1, Quality = 1 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(0));
-    }
-
-    [TestCase(8, 4)]
-    [TestCase(4, 0)]
-    public void ItShouldDecreaseQualityTwiceAsFastAfterSellIn(int initialQuality, int expectedQuality)
-    {
-        Item item = new Item { Name = "Conjured", SellIn = 0, Quality = initialQuality };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
-    }
-}
-
 public class GildedRoseTest
 {
     [Test]

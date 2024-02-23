41dc7574ec9d2ac37904707552db55440d13c3c2
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 15:22:13 +0000

    refactor: Move aged brie tests to own file

---
+++ GildedRoseTests/AgedBrieTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class AgedBrieTests
+{
+    [TestCase(2,1)]
+    [TestCase(1,0)]
+    [TestCase(0, -1)]
+    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
+    }
+
+    [TestCase(48, 49)]
+    [TestCase(49, 50)]
+    public void ItShouldIncreaseQuality(int initialQuality, int expectedQuality)
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = 1, Quality = initialQuality };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [Test]
+    public void ItShouldIncreaseInQualityTwiceAsFastWhenPastSellIn()
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void ItShouldNotIncreaseQualityAbove50()
+    {
+        Item item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(50));
+    }
+}
--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -42,56 +42,6 @@ Just for clarification, an item can never have its Quality increase above 50, ho
 a legendary item and as such its Quality is 80, and it never alters.
  */
 
-public class AgedBrieTests
-{
-    [TestCase(2,1)]
-    [TestCase(1,0)]
-    [TestCase(0, -1)]
-    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
-    {
-        Item item = new Item { Name = "Aged Brie", SellIn = initialSellIn, Quality = 0 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
-    }
-
-    [TestCase(48, 49)]
-    [TestCase(49, 50)]
-    public void ItShouldIncreaseQuality(int initialQuality, int expectedQuality)
-    {
-        Item item = new Item { Name = "Aged Brie", SellIn = 1, Quality = initialQuality };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
-    }
-
-    [Test]
-    public void ItShouldIncreaseInQualityTwiceAsFastWhenPastSellIn()
-    {
-        Item item = new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(2));
-    }
-
-    [Test]
-    public void ItShouldNotIncreaseQualityAbove50()
-    {
-        Item item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 };
-
-        GildedRose gildedRose = new GildedRose(new List<Item> { item });
-        gildedRose.UpdateQuality();
-
-        Assert.That(item.Quality, Is.EqualTo(50));
-    }
-}
-
 public class GildedRoseTest
 {
     [Test]

6739643ded6e605efa5049992dbbc7e326fb4e0e
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 11:41:27 +0000

    refactor: Use compund assignments

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -27,7 +27,7 @@ public class GildedRose
             {
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    item.Quality = item.Quality - 1;
+                    item.Quality--;
                 }
             }
         }
@@ -35,7 +35,7 @@ public class GildedRose
         {
             if (item.Quality < 50)
             {
-                item.Quality = item.Quality + 1;
+                item.Quality++;
 
                 if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
@@ -43,7 +43,7 @@ public class GildedRose
                     {
                         if (item.Quality < 50)
                         {
-                            item.Quality = item.Quality + 1;
+                            item.Quality++;
                         }
                     }
 
@@ -51,7 +51,7 @@ public class GildedRose
                     {
                         if (item.Quality < 50)
                         {
-                            item.Quality = item.Quality + 1;
+                            item.Quality++;
                         }
                     }
                 }
@@ -60,7 +60,7 @@ public class GildedRose
 
         if (item.Name != "Sulfuras, Hand of Ragnaros")
         {
-            item.SellIn = item.SellIn - 1;
+            item.SellIn--;
         }
 
         if (item.SellIn < 0)
@@ -73,20 +73,20 @@ public class GildedRose
                     {
                         if (item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            item.Quality = item.Quality - 1;
+                            item.Quality--;
                         }
                     }
                 }
                 else
                 {
-                    item.Quality = item.Quality - item.Quality;
+                    item.Quality -= item.Quality;
                 }
             }
             else
             {
                 if (item.Quality < 50)
                 {
-                    item.Quality = item.Quality + 1;
+                    item.Quality++;
                 }
             }
         }

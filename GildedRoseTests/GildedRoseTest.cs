844a8aa3182ec81c6736fc499d502c5da77b0fdc
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 16:15:47 +0000

    test: Add tests for default item

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -42,6 +42,57 @@ Just for clarification, an item can never have its Quality increase above 50, ho
 a legendary item and as such its Quality is 80, and it never alters.
  */
 
+public class DefaultItemTests
+{
+    [TestCase(1, 0)]
+    [TestCase(0, -1)]
+    public void ItShouldDecreaseSellIn(int initialSellIn, int expectedSellIn)
+    {
+        Item item = new Item { Name = "foo", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
+    }
+
+    [TestCase(2, 1)]
+    [TestCase(1, 0)]
+    public void ItShouldDecreaseQuality(int initialQuality, int expectedQuality)
+    {
+        Item item = new Item { Name = "foo", SellIn = 1, Quality = initialQuality };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [TestCase(0, 0)]
+    [TestCase(-1, 0)]
+    public void ItShouldDecreaseQualityTwiceAsFastAfterSellIn(int initialSellIn, int expectedQuality)
+    {
+        Item item = new Item { Name = "foo", SellIn = initialSellIn, Quality = 2 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
+    [TestCase(0, 0)]
+    [TestCase(-1, 0)]
+    public void ItShouldNotDecreaseQualityBelowZero(int initialSellIn, int expectedQuality)
+    {
+        Item item = new Item { Name = "foo", SellIn = initialSellIn, Quality = 0 };
+
+        GildedRose gildedRose = new GildedRose(new List<Item> { item });
+        gildedRose.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+}
+
 public class GildedRoseTest
 {
     [Test]

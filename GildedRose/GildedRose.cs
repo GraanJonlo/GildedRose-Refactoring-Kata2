b2836060c28958b62960b3163d389ecd070b0a39
Andrew Grant <andrew.grant@uinsure.co.uk>
2024-02-23 12:45:02 +0000

    refactor: Move update item logic to wrapped item type

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -2,16 +2,6 @@
 
 namespace GildedRoseKata;
 
-public class WrappedItem
-{
-    public readonly Item _item;
-
-    public WrappedItem(Item item)
-    {
-        _item = item;
-    }
-}
-
 public class GildedRose(IList<Item> items)
 {
     public void UpdateQuality()
@@ -19,79 +9,7 @@ public class GildedRose(IList<Item> items)
         foreach (var item in items)
         {
             WrappedItem wrappedItem = new(item);
-            UpdateItem(item);
-        }
-    }
-
-    private static void UpdateItem(Item item)
-    {
-        if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (item.Quality > 0)
-            {
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality--;
-                }
-            }
-        }
-        else
-        {
-            if (item.Quality < 50)
-            {
-                item.Quality++;
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.SellIn < 11)
-                    {
-                        IncreaseQuality(item);
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        IncreaseQuality(item);
-                    }
-                }
-            }
-        }
-
-        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        {
-            item.SellIn--;
-        }
-
-        if (item.SellIn < 0)
-        {
-            if (item.Name != "Aged Brie")
-            {
-                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality--;
-                        }
-                    }
-                }
-                else
-                {
-                    item.Quality -= item.Quality;
-                }
-            }
-            else
-            {
-                IncreaseQuality(item);
-            }
-        }
-    }
-
-    private static void IncreaseQuality(Item item)
-    {
-        if (item.Quality < 50)
-        {
-            item.Quality++;
+            wrappedItem.UpdateItem();
         }
     }
 }
---
+++ GildedRose/WrappedItem.cs
@@ -0,0 +1,76 @@
+﻿namespace GildedRoseKata;
+
+public class WrappedItem(Item item)
+{
+    public void UpdateItem()
+    {
+        if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+        {
+            if (item.Quality > 0)
+            {
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality--;
+                }
+            }
+        }
+        else
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn < 11)
+                    {
+                        IncreaseQuality();
+                    }
+
+                    if (item.SellIn < 6)
+                    {
+                        IncreaseQuality();
+                    }
+                }
+            }
+        }
+
+        if (item.Name != "Sulfuras, Hand of Ragnaros")
+        {
+            item.SellIn--;
+        }
+
+        if (item.SellIn < 0)
+        {
+            if (item.Name != "Aged Brie")
+            {
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality--;
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality -= item.Quality;
+                }
+            }
+            else
+            {
+                IncreaseQuality();
+            }
+        }
+    }
+
+    private void IncreaseQuality()
+    {
+        if (item.Quality < 50)
+        {
+            item.Quality++;
+        }
+    }
+}
